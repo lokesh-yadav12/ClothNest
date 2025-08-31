1f81c567da4d5a09d1c139506d30797b21a05ed2
lokesh-yadav12 <lokesh916635@gmail.com>
2025-08-31 23:20:07 +0530

    Update variefy.jsx

--- frontend/src/pages/variefy.jsx
+++ frontend/src/pages/variefy.jsx
@@ -1,45 +1,45 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { ShopContext } from '../context/ShopContext'
-import axios from 'axios'
-import { toast } from 'react-toastify'
-import { useParams } from 'react-router-dom'
+import React, { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useParams } from "react-router-dom";
 
 const variefy = () => {
+  const { navigate, token, setCartItems, backendUrl } = useContext(ShopContext);
+  const [searchParams, setSearchParams] = useState();
+  const success = searchParams.get("success");
+  const orderId = searchParams.get("orderId");
 
-    const {navigate,token,setCartItems,backendUrl} = useContext(ShopContext)
-    const [searchParams,setSearchParams] = useState()
-    const success = searchParams.get('success')
-    const orderId = searchParams.get('orderId')
+  const variefyPayment = async () => {
+    try {
+      if (!token) {
+        return null;
+      }
 
-    const variefyPayment = async()=>{
-            try {
-                if(!token){
-                    return null
-                }
-
-                const response = await axios.post(backendUrl+'/api/order/variefyStripe',{success,orderId},{headers:{token}})
-                if(response.data.success){
-                    setCartItems({})
-                    navigate('/orders')
-                }
-                else{
-                    navigate('/cart')
-                }
-            } catch (error) {
-                console.log(error)
-                toast.error(error.message)
-            }
+      const response = await axios.post(
+        backendUrl + "/api/order/variefyStripe",
+        { success, orderId },
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      if (response.data.success) {
+        setCartItems({});
+        navigate("/orders");
+      } else {
+        navigate("/cart");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
     }
+  };
 
-    useEffect(()=>{
-        variefyPayment();
-    },[token])
+  useEffect(() => {
+    variefyPayment();
+  }, [token]);
 
-  return (
-    <div>
+  return <div></div>;
+};
 
-    </div>
-  )
-}
-
-export default variefy
+export default variefy;
