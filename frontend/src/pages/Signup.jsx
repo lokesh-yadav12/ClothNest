88db29df8d22a277bcd9916463faa3d0f65c723c
lokesh-yadav12 <lokesh916635@gmail.com>
2025-09-08 19:38:38 +0530

    Create Signup.jsx

---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,67 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { ShopContext } from '../context/ShopContext';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import { useNavigate, Link } from 'react-router-dom';
+
+const Signup = () => {
+  const { token, setToken, backendUrl } = useContext(ShopContext);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+    try {
+     const response = await axios.post(
+  `${backendUrl}/api/user/register`,
+  { name, email, password },
+  {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    withCredentials: true, // helps if cookies are used later
+  }
+);
+
+      if (response.data.success) {
+        setToken(response.data.token);
+        localStorage.setItem('token', response.data.token);
+        navigate('/');
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response?.data?.message || error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (token) navigate('/');
+  }, [token, navigate]);
+
+  return (
+    <form onSubmit={onSubmitHandler} className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800">
+      <div className="inline-flex items-center gap-2 mb-2 mt-10">
+        <p className="prata-regular text-3xl">Sign Up</p>
+        <hr className="border-none h-[1.5px] w-8 bg-gray-800" />
+      </div>
+
+      <input onChange={(e) => setName(e.target.value)} value={name} type="text" className="w-full px-3 py-2 border border-gray-800" placeholder="Name" required />
+      <input onChange={(e) => setEmail(e.target.value)} value={email} type="email" className="w-full px-3 py-2 border border-gray-800" placeholder="Email" required />
+      <input onChange={(e) => setPassword(e.target.value)} value={password} type="password" className="w-full px-3 py-2 border border-gray-800" placeholder="Password" required />
+
+      <div className="w-full justify-between flex text-sm mt-[-8px]">
+        <Link to="/login" className="cursor-pointer">Already have an account? Login</Link>
+      </div>
+
+      <button className="bg-black text-white font-light px-8 py-2 mt-4">
+        Sign Up
+      </button>
+    </form>
+  );
+};
+
+export default Signup;
